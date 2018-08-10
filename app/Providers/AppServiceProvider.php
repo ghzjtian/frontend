a621df40c461e56e8a0faeb2356d6fd256421f30
tianzeng <wyzjtian@163.com>
2018-08-10 18:44:27 +0800

    1.fix  '1071 Specified key was too long; max key length is 767 bytes' error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -16,6 +17,10 @@ class AppServiceProvider extends ServiceProvider
     {
 //        https://laravel-china.org/docs/laravel/5.6/blade/1375#displaying-data
 //        Blade::withoutDoubleEncoding();
+
+        // 1071 Specified key was too long; max key length is 767 bytes
+        //https://laravel-china.org/articles/4195/laravel-54-common-error-specified-key-was-too-long
+        Schema::defaultStringLength(191);
     }
 
     /**
