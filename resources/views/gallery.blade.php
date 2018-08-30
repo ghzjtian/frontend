5c3a8c4d944b53964515fd0058e47494eb03210a
tianzeng <wyzjtian@163.com>
2018-08-30 14:59:04 +0800

    1.更改了title 为选中的项目的名字

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.base')
-{{--图片展示,从 gallery 中选几张酒的作品出来这里展示，点击进去就是 gallery 页}}
-{{-- gallery 信息--}}
+
+@section('page_title',setting('glb.gallery'))
+
 @section('gallery')
     @include('layouts.gallery',['isHomePage'=>false])
 @endsection
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.base')
 
+@section('page_title',setting('glb.products'))
+
 {{--产品展示--}}
 @section('products')
     @include('layouts.products',['isHomePage'=>false])
